8d17f4f7f7ac5ae3332b82586963a2be94126de2
Felix Plazek (U555994) <felix.plazek@lhind.dlh.de>
2018-06-04 15:34:05 +0200

    initial commit; working multi module maven project including angular and spring boot

---
+++ backend/src/main/java/de/nordakademie/flxplzk/transferleistungangular/TransferleistungAngularApplication.java
@@ -0,0 +1,12 @@
+package de.nordakademie.flxplzk.transferleistungangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferleistungAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferleistungAngularApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/de/nordakademie/flxplzk/transferleistungangular/TransferleistungAngularApplicationTests.java
@@ -0,0 +1,16 @@
+package de.nordakademie.flxplzk.transferleistungangular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransferleistungAngularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
