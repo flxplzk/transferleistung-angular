d77ff36d80fb8b4b96891081b1222c9c171d9960
Felix Plazek (U555994) <felix.plazek@lhind.dlh.de>
2018-07-02 11:46:07 +0200

    vaadin overview is implemented and refactored using declarative design

--- backend/src/main/java/de/nordakademie/flxplzk/transferleistungangular/vaadin/ui/components/TaskComponent.java
+++ backend/src/main/java/de/nordakademie/flxplzk/transferleistungangular/vaadin/ui/components/TaskComponent.java
@@ -2,23 +2,39 @@ package de.nordakademie.flxplzk.transferleistungangular.vaadin.ui.components;
 
 
 import com.vaadin.annotations.DesignRoot;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.ClientConnector;
+import com.vaadin.shared.Registration;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.declarative.Design;
+import com.vaadin.ui.themes.ValoTheme;
 import de.nordakademie.flxplzk.transferleistungangular.domain.Task;
 
 public class TaskComponent extends CustomComponent {
     private TaskDesign designRoot = new TaskDesign();
+
     @DesignRoot
-    private static class TaskDesign extends VerticalLayout{
+    private static class TaskDesign extends CssLayout{
         private Label title;
         private Label description;
+        private Button trashButton;
     }
+
     public TaskComponent(Task task) {
         Design.read("TaskComponent.html", this.designRoot);
         setCompositionRoot(this.designRoot);
         designRoot.title.setValue(task.getTitle());
         designRoot.description.setValue(task.getDescription());
+        designRoot.trashButton.setIcon(VaadinIcons.TRASH);
+        designRoot.trashButton.setStyleName(ValoTheme.BUTTON_BORDERLESS);
+    }
+
+    public Registration addDeleteListener(Button.ClickListener listener) {
+        return this.designRoot.trashButton.addClickListener(listener);
     }
 }
--- backend/src/main/java/de/nordakademie/flxplzk/transferleistungangular/vaadin/ui/components/TaskView.java
+++ backend/src/main/java/de/nordakademie/flxplzk/transferleistungangular/vaadin/ui/components/TaskView.java
@@ -1,6 +1,8 @@
 package de.nordakademie.flxplzk.transferleistungangular.vaadin.ui.components;
 
+import com.vaadin.annotations.DesignRoot;
 import com.vaadin.navigator.View;
+import com.vaadin.shared.Registration;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
@@ -9,41 +11,61 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.declarative.Design;
 import com.vaadin.ui.themes.ValoTheme;
+import de.nordakademie.flxplzk.transferleistungangular.domain.Task;
 import de.nordakademie.flxplzk.transferleistungangular.service.TaskService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringView(name = "")
 public class TaskView extends CustomComponent implements View {
 
+    @DesignRoot
+    private static class TaskViewDesign extends VerticalLayout {
+        private TextField searchTerm;
+        private Button searchButton;
+        private Button newTaskButton;
+        private VerticalLayout tasksContainer;
+    }
+
     private final TaskService taskService;
-    private Label titel = new Label("Meine Aufgaben");
-    private TextField searchTerm = new TextField();
-    private Button searchButton = new Button("suchen");
-    private VerticalLayout tasksContainer = new VerticalLayout();
-    private Button newTaskButton = new Button("Neue Aufgabe anlegen");
-    private HorizontalLayout searchPanel = new HorizontalLayout(searchTerm, searchButton);
-    private VerticalLayout rootLayout = new VerticalLayout(titel, searchPanel, tasksContainer, newTaskButton);
+    private final TaskViewDesign rootComponent = new TaskViewDesign();
+    private final List<Registration> registrations = new ArrayList<>();
 
     public TaskView(final TaskService taskService) {
+        Design.read("TaskView.html", this.rootComponent);
         this.taskService = taskService;
-        setCompositionRoot(rootLayout);
-        taskService.findAll().forEach(task ->
-                tasksContainer.addComponent(
-                        new TaskComponent(task)
-                )
-        );
-        searchPanel.setExpandRatio(searchTerm, 1f);
-        searchPanel.setWidth(100, Unit.PERCENTAGE);
-        searchTerm.setWidth(100, Unit.PERCENTAGE);
-        tasksContainer.setHeightUndefined();
-        tasksContainer.setWidth("100%");
-        tasksContainer.setSpacing(true);
-        rootLayout.setComponentAlignment(newTaskButton, Alignment.BOTTOM_RIGHT);
-        newTaskButton.addStyleName(ValoTheme.BUTTON_FRIENDLY);
-        rootLayout.setSizeFull();
-        titel.addStyleName(ValoTheme.LABEL_H2);
-        titel.addStyleName(ValoTheme.LABEL_BOLD);
-        rootLayout.setExpandRatio(tasksContainer, 1f);
-        setSizeFull();
+        setCompositionRoot(rootComponent);
+        findTasks();
+        attachListeners();
+    }
+
+    private void attachListeners() {
+        rootComponent.searchButton.addClickListener(click -> findTasks());
+        rootComponent.searchTerm.addValueChangeListener(change -> findTasks());
+    }
+
+    private void findTasks() {
+        removeRegistrations();
+        rootComponent.tasksContainer.removeAllComponents();
+        this.taskService.findAll().forEach(task -> {
+            final TaskComponent taskComponent = new TaskComponent(task);
+            rootComponent.tasksContainer.addComponent(taskComponent);
+            this.registrations.add(taskComponent.addDeleteListener(click -> {
+                delete(task);
+                findTasks();
+            }));
+        });
+    }
+
+    private void delete(Task task) {
+        this.taskService.delete(task.getId());
+    }
+
+    private void removeRegistrations() {
+        this.registrations.forEach(Registration::remove);
+        this.registrations.clear();
     }
 }
